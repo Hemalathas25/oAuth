2b3384275193432299a0fecd8c645eb9f7024691
Hemalatha Sakthivel <hemalathas@canvendor.com>
2024-10-17 12:00:57 +0530

    Modified

--- client/src/component/Header.js
+++ client/src/component/Header.js
@@ -1,17 +1,31 @@
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { FaUser } from 'react-icons/fa';
+import { LinkContainer } from 'react-router-bootstrap';
+
 
 
 const Header = () => {
+
   return (
     <header>
         <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
             <Container>
-                <Navbar.Brand href="/">Canvendor</Navbar.Brand>
+                
+                <Navbar.Brand href="/">
+                Canvendor
+                </Navbar.Brand>
+
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ms-auto">
-                        <Nav.Link href="/login"><FaUser /> Sign in</Nav.Link>
+
+                    <LinkContainer to='/login'>
+                    <Nav.Link href="/login">
+                    <FaUser /> Sign In
+                    </Nav.Link>
+                    </LinkContainer>
+
+
                     </Nav>
                 </Navbar.Collapse>
             </Container>
